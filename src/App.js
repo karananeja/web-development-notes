2d54054b75cc76c859e491d27bb68f8ecc4c32a9
karananeja <karananeja.r@gmail.com>
2022-09-18 13:04:44 +0530

    added different pages for the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import JavaScriptComponent from './pages/JavaScript';
+import NodeComponent from './pages/Node';
+import OopsConceptComponent from './pages/OopsConcept';
+import ReactComponent from './pages/React';
 
 const App = () => {
   return (
     <div className='app'>
-      Web Development Notes
+      <Routes>
+        {/* Using react route nesting to group the routes will similar path */}
+        <Route path='/' >
+          <Route index element={<Home />} />
+          <Route path='oopsconcepts' element={<OopsConceptComponent />} />
+          <Route path='javascript' element={<JavaScriptComponent />} />
+          <Route path='react' element={<ReactComponent />} />
+          <Route path='node' element={<NodeComponent />} />
+        </Route>
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
     </div >
   )
 }
