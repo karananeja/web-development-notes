6a3be81d42264a2fa9ac48f03f69e5cb3113e5e9
karananeja <karananeja.r@gmail.com>
2022-09-24 22:44:27 +0530

    added the home page to the app

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h4 className='home__title'>
+        Web Development Notes
+      </h4>
+      <div className='home__links'>
+        <Link className='home__link' to='/oopsconcepts'>Oops Concepts Notes</Link>
+        <Link className='home__link' to='/javascript'>JavaScript Notes</Link>
+        <Link className='home__link' to='/react'>React Notes</Link>
+        <Link className='home__link' to='/node'>Node Notes</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
