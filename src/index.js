030963ec17af594c7d00d33fc247201d46bc602e
karananeja <karananeja.r@gmail.com>
2022-12-06 09:34:27 +0530

    rectified spelling error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Wrap the entier application inside the BrowserRouter Component */}
+    {/* Wrap the entire application inside the BrowserRouter Component */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
