5b9d3bc20d7ac40d7cc2722c3ca34b6e39cf1785
karananeja <karananeja.r@gmail.com>
2023-07-09 23:17:47 +0530

    formatted the notes files

--- src/pages/JavaScript.js
+++ src/pages/JavaScript.js
@@ -12,16 +12,24 @@ const JavaScriptComponent = () => {
             It was created by <strong>Brendan Eich</strong> in <i>1995</i>
           </li>
           <li>
-            It was initially named <strong><i>Mocha</i></strong>
+            It was initially named{' '}
+            <strong>
+              <i>Mocha</i>
+            </strong>
           </li>
           <li>
-            It is an OOP language commonly used to create interactive effects within web browsers
+            It is an OOP language commonly used to create interactive effects
+            within web browsers
             <ul>
               <li>
-                <strong>Java</strong> is complete programming language whereas <strong>JavaScript</strong> is a coded program that can be introduced to HTML pages
+                <strong>Java</strong> is complete programming language whereas{' '}
+                <strong>JavaScript</strong> is a coded program that can be
+                introduced to HTML pages
               </li>
               <li>
-                <strong>Java</strong> is an OOPS or structured programming language like <strong>C++</strong> or <strong>C</strong> whereas <strong>JavaScript</strong> is a client-side scripting language
+                <strong>Java</strong> is an OOPS or structured programming
+                language like <strong>C++</strong> or <strong>C</strong> whereas{' '}
+                <strong>JavaScript</strong> is a client-side scripting language
               </li>
             </ul>
           </li>
@@ -49,42 +57,49 @@ const JavaScriptComponent = () => {
             </ul>
           </li>
           <li>
-            The famous JavaScript keyword <i>this</i> refers to the current context
+            The famous JavaScript keyword <i>this</i> refers to the current
+            context
           </li>
           <li>
             In Strict Mode <i>this</i> is undefined
           </li>
           <li>
-            A Callback is a plain JavaScript function passed to some method as an argument or option to be executed later
+            A Callback is a plain JavaScript function passed to some method as
+            an argument or option to be executed later
           </li>
           <li>
-            Closures are created whenever a variable that is defined outside the current scope is accessed from within some inner scope
+            Closures are created whenever a variable that is defined outside the
+            current scope is accessed from within some inner scope
           </li>
           <li>
             Variable Naming Convention
             <ul>
               <li>
-                Do not use any of the JavaScript reserved keyword as a variable name
+                Do not use any of the JavaScript reserved keyword as a variable
+                name
               </li>
               <li>
-                JavaScript variable names should not start with <strong>Number</strong>
+                JavaScript variable names should not start with{' '}
+                <strong>Number</strong>
               </li>
-              <li>
-                They are case sensitive
-              </li>
+              <li>They are case sensitive</li>
             </ul>
           </li>
           <li>
-            Attributes provide more details on an element like id, type, value etc
+            Attributes provide more details on an element like id, type, value
+            etc
           </li>
           <li>
-            Property is the value assigned to the property like type='text', value='name' etc
+            Property is the value assigned to the property like type='text',
+            value='name' etc
           </li>
           <li>
-            JavaScript is dynamically typed language and works Just-in-time compilation
+            JavaScript is dynamically typed language and works Just-in-time
+            compilation
           </li>
           <li>
-            Local Storage will stay until it is manually cleared through settings or program
+            Local Storage will stay until it is manually cleared through
+            settings or program
           </li>
           <li>Maximum limit of data saving in Local Storage is about 5 MB.</li>
           <li>Session storage will leave when the browser is closed</li>
@@ -99,64 +114,72 @@ const JavaScriptComponent = () => {
             while restoring the tab. It is the browser dependent feature while
             restoring the tab.
           </li>
+          <li>innerHTML - It will process an HTML tag if found in a string</li>
           <li>
             innerText - It will not process an HTML tag if found in a string
           </li>
           <li>
-            Strict mode is a way to introduce better error-checking into the code
+            Strict mode is a way to introduce better error-checking into the
+            code
           </li>
           <li>
-            <strong><i>this</i></strong> keyword refers to the object that the method is associated with
+            <strong>
+              <i>this</i>
+            </strong>{' '}
+            keyword refers to the object that the method is associated with
           </li>
           <li>
-            Two most important parts of the browser are the JavaScript Engine and a Rendering Engine (BLINK)
+            Two most important parts of the browser are the JavaScript Engine
+            and a Rendering Engine (BLINK)
           </li>
           <li>
-            Functions running in parallel with other functions are known as <strong>Asynchronous Functions</strong>
+            Functions running in parallel with other functions are known as{' '}
+            <strong>Asynchronous Functions</strong>
           </li>
           <li>
-            <h3>
-              Promises
-            </h3>
+            <h3>Promises</h3>
             <ul>
               <li>
-                Promise Object contains both the producing code and calls the consuming code
+                Promise Object contains both the producing code and calls the
+                consuming code
               </li>
               <li>
                 Benefits of using Promises
                 <ul>
-                  <li>
-                    Improves code readability
-                  </li>
-                  <li>
-                    Better handling of Asynchronous operations
-                  </li>
-                  <li>
-                    Better error handling
-                  </li>
+                  <li>Improves code readability</li>
+                  <li>Better handling of Asynchronous operations</li>
+                  <li>Better error handling</li>
                 </ul>
               </li>
               <li>
                 There are four states in Promises
                 <ul>
-                  <li><i>Fulfilled</i></li>
-                  <li><i>Rejected</i></li>
-                  <li><i>Pending</i></li>
-                  <li><i>Settled</i></li>
+                  <li>
+                    <i>Fulfilled</i>
+                  </li>
+                  <li>
+                    <i>Rejected</i>
+                  </li>
+                  <li>
+                    <i>Pending</i>
+                  </li>
+                  <li>
+                    <i>Settled</i>
+                  </li>
                 </ul>
               </li>
               <li>
-                It can be consumed by registering function using <i>.then()</i> and <i>.catch()</i> methods
+                It can be consumed by registering function using <i>.then()</i>{' '}
+                and <i>.catch()</i> methods
               </li>
               <li>
-                <i>.then()</i> is invoked when a promise is either rejected or resolved
+                <i>.then()</i> is invoked when a promise is either rejected or
+                resolved
               </li>
               <li>
                 Application of Promises
                 <ul>
-                  <li>
-                    It is used for Asynchronous handling of events
-                  </li>
+                  <li>It is used for Asynchronous handling of events</li>
                   <li>
                     It is used to handle Asynchronous <i>http</i> requests
                   </li>
@@ -165,27 +188,23 @@ const JavaScriptComponent = () => {
             </ul>
           </li>
           <li>
-            <h4>
-              Promises vs Async/Await
-            </h4>
+            <h4>Promises vs Async/Await</h4>
             <table className='javaScriptComponent__table'>
               <thead>
                 <tr>
-                  <th>
-                    Promise
-                  </th>
-                  <th>
-                    Async/Await
-                  </th>
+                  <th>Promise</th>
+                  <th>Async/Await</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>
-                    Promise is an object representing intermediate state of operation which is guaranteed to complete its execution
+                    Promise is an object representing intermediate state of
+                    operation which is guaranteed to complete its execution
                   </td>
                   <td>
-                    Async/Await is a syntactic sugar for promises, a code wrapper making the code execute more synchronously
+                    Async/Await is a syntactic sugar for promises, a code
+                    wrapper making the code execute more synchronously
                   </td>
                 </tr>
                 <tr>
@@ -193,15 +212,18 @@ const JavaScriptComponent = () => {
                     It has 4 states - Resolved, Rejected, Pending and Settled
                   </td>
                   <td>
-                    It doesn't have any states, it returns a promise either resolved or rejected
+                    It doesn't have any states, it returns a promise either
+                    resolved or rejected
                   </td>
                 </tr>
                 <tr>
                   <td>
-                    Error handling is done using <i>.then()</i> and <i>.catch()</i> methods
+                    Error handling is done using <i>.then()</i> and{' '}
+                    <i>.catch()</i> methods
                   </td>
                   <td>
-                    Error handling is done using <i>.then()</i> and <i>.catch()</i> methods
+                    Error handling is done using <i>.then()</i> and{' '}
+                    <i>.catch()</i> methods
                   </td>
                 </tr>
                 <tr>
@@ -215,59 +237,43 @@ const JavaScriptComponent = () => {
               </tbody>
             </table>
           </li>
+          <li>Callback Hell is also known as the Pyramid of Doom</li>
           <li>
-            Callback Hell is also known as the Pyramid of Doom
+            It is a pattern caused by intensively nested callbacks which are
+            unreadable
           </li>
           <li>
-            It is a pattern caused by intensively nested callbacks which are unreadable
-          </li>
-          <li>
-            It typically contains multiple nested callback functions which in turn make the code hard to read and debug
+            It typically contains multiple nested callback functions which in
+            turn make the code hard to read and debug
           </li>
           <li>
             <h4>
-              Difference between <strong>Var</strong>, <strong>Let</strong> and <strong>Const</strong>
+              Difference between <strong>Var</strong>, <strong>Let</strong> and{' '}
+              <strong>Const</strong>
             </h4>
             <table className='javaScriptComponent__table'>
               <thead>
                 <tr>
-                  <th>
-                    Var
-                  </th>
-                  <th>
-                    Let
-                  </th>
-                  <th>
-                    Const
-                  </th>
+                  <th>Var</th>
+                  <th>Let</th>
+                  <th>Const</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>
-                    It is globally scoped or function scoped
-                  </td>
-                  <td>
-                    It is block scoped
-                  </td>
-                  <td>
-                    It is block scoped
-                  </td>
+                  <td>It is globally scoped or function scoped</td>
+                  <td>It is block scoped</td>
+                  <td>It is block scoped</td>
+                </tr>
+                <tr>
+                  <td>It can be declared and updated again</td>
+                  <td>It can be updated again but not declared once again</td>
+                  <td>It neither can be declared nor updated again</td>
                 </tr>
                 <tr>
                   <td>
-                    It can be declared and updated again
-                  </td>
-                  <td>
-                    It can be updated again but not declared once again
-                  </td>
-                  <td>
-                    It neither can be declared nor updated again
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    It is hoisted to the top of the scope and initialized with <i>undefined</i> value
+                    It is hoisted to the top of the scope and initialized with{' '}
+                    <i>undefined</i> value
                   </td>
                   <td>
                     It is hoisted to the top of the scope but not initialized
@@ -277,22 +283,18 @@ const JavaScriptComponent = () => {
                   </td>
                 </tr>
                 <tr>
-                  <td>
-                    It can declared without being initialized
-                  </td>
-                  <td>
-                    It can declared without being initialized
-                  </td>
-                  <td>
-                    It cannot declared without being initialized
-                  </td>
+                  <td>It can declared without being initialized</td>
+                  <td>It can declared without being initialized</td>
+                  <td>It cannot declared without being initialized</td>
                 </tr>
               </tbody>
             </table>
           </li>
         </ul>
       </div>
-      <Link className='javaScriptComponent__link' to='/'>Back to Home</Link>
+      <Link className='javaScriptComponent__link' to='/'>
+        Back to Home
+      </Link>
     </div>
   );
 };
--- src/pages/OopsConcept.js
+++ src/pages/OopsConcept.js
@@ -7,83 +7,112 @@ const OopsConceptComponent = () => {
     <div className='oopsConceptComponent'>
       <div className='oopsConceptComponent__details'>
         <h1>Object Oriented Programming (OOP)</h1>
-        It is a programming paradigm (model) based on the concepts of <strong>objects</strong> which can contain data and code, data in the form of fields and and code in the form of procedure <br />
+        It is a programming paradigm (model) based on the concepts of{' '}
+        <strong>objects</strong> which can contain data and code, data in the
+        form of fields and and code in the form of procedure <br />
         OOP facilitates software reuse, potentially shortening development time
         <h3 className='oopsConceptComponent__titleOne'>Pillars</h3>
         <ul>
-          <li>
-            Inheritance
-          </li>
-          <li>
-            Polymorphism
-          </li>
-          <li>
-            Encapsulation
-          </li>
-          <li>
-            Abstraction
-          </li>
+          <li>Inheritance</li>
+          <li>Polymorphism</li>
+          <li>Encapsulation</li>
+          <li>Abstraction</li>
         </ul>
         <div className='oopsConceptComponent__pillarOne'>
           <h4 className='oopsConceptComponent__titleTwo'>Inheritance</h4>
           <p>
-            It is form of software reuse in which a new class is created by absorbing an existing class's members and embellishing them with new or modified capabilities. <br />
-            Existing class is called as Super Class (Parent Class), New class is called as Sub Class (Child Class) <br />
-            Direct Super Class - is the superclass from which subclass explicitly extends <br />
-            Indirect Super Class - it is any class above the direct superclass in the class hierarchy which defines the inheritance relationship between classes <br />
-            Super Class - general info - set of objects represented - larger <br />
-            Sub Class - specific info - set of objects represented - smaller <br />
+            It is form of software reuse in which a new class is created by
+            absorbing an existing class's members and embellishing them with new
+            or modified capabilities. <br />
+            Existing class is called as Super Class (Parent Class), New class is
+            called as Sub Class (Child Class) <br />
+            Direct Super Class - is the superclass from which subclass
+            explicitly extends <br />
+            Indirect Super Class - it is any class above the direct superclass
+            in the class hierarchy which defines the inheritance relationship
+            between classes <br />
+            Super Class - general info - set of objects represented - larger{' '}
+            <br />
+            Sub Class - specific info - set of objects represented - smaller{' '}
+            <br />
             <ul>
               <li>
-                The first task of any sub class constructor is to call its direct superclass's constructor, either explicitly or implicitly
+                The first task of any sub class constructor is to call its
+                direct superclass's constructor, either explicitly or implicitly
               </li>
               <li>
-                If the code doesnot include an explicit call to the superclass constructor, Java implicitly calls the superclass's default or no-argument constructor if it doesnot exists, then the compiler would throw an error
+                If the code does not include an explicit call to the superclass
+                constructor, Java implicitly calls the superclass's default or
+                no-argument constructor if it does not exists, then the compiler
+                would throw an error
               </li>
               <li>
-                To override a superclass method, a sub class must declare a method with same signature (method name, number of parameters, parameter types and order of parameter types) as of the superclass method
+                To override a superclass method, a sub class must declare a
+                method with same signature (method name, number of parameters,
+                parameter types and order of parameter types) as of the
+                superclass method
               </li>
               <li>
-                Explicit call to superclass constructor can be done by using <strong>super()</strong> method
+                Explicit call to superclass constructor can be done by using{' '}
+                <strong>super()</strong> method
               </li>
               <li>
                 Private superclass members cannot be accessed in a sub class
               </li>
               <li>
-                In Single Inheritance, a class is derived from one direct superclass, In Multiple Inheritance, a class is derived from more than one direct superclass (Java doesnot support Multiple Inheritance)
+                In Single Inheritance, a class is derived from one direct
+                superclass, In Multiple Inheritance, a class is derived from
+                more than one direct superclass (Java does not support Multiple
+                Inheritance)
               </li>
               <li>
-                Multi-level Inheritance example - Teacher's class derived from Faculty Class. which is further derived from Committee Class
+                Multi-level Inheritance example - Teacher's class derived from
+                Faculty Class. which is further derived from Committee Class
               </li>
               <li>
-                To overcome Multiple Inheritances problem in Java, we can use <strong>Interfaces</strong>
+                To overcome Multiple Inheritances problem in Java, we can use{' '}
+                <strong>Interfaces</strong>
               </li>
               <li>
-                Hierarchial Inheritance example - Teaching staff class derived from Committee Class and Non-Teaching Class derived from Committee Class as well
+                Hierarchial Inheritance example - Teaching staff class derived
+                from Committee Class and Non-Teaching Class derived from
+                Committee Class as well
               </li>
               <li>
-                Hybrid Inheritance - Combination of single and multiple inheritance (not allowed in Java)
+                Hybrid Inheritance - Combination of single and multiple
+                inheritance (not allowed in Java)
               </li>
               <li>
-                <h5 className='oopsConceptComponent__titleThree'>Constructors</h5>
+                <h5 className='oopsConceptComponent__titleThree'>
+                  Constructors
+                </h5>
                 <ul>
+                  <li>Constructors are not inherited</li>
                   <li>
-                    Constructors are not inherited
+                    Every object can access a reference to itself with{' '}
+                    <strong>
+                      <i>this</i>
+                    </strong>{' '}
+                    keyword
                   </li>
                   <li>
-                    Every object can access a reference to itself with <strong><i>this</i></strong> keyword
+                    <strong>
+                      <i>this</i>
+                    </strong>{' '}
+                    keyword cannot be used in static method but can be used in
+                    non-static method
+                  </li>
+                  <li>Every class must have at least one constructor</li>
+                  <li>
+                    If we do not provide any constructors in a class's
+                    declaration, then the compiler creates a default constructor
+                    that takes no arguments when it is invoked
                   </li>
                   <li>
-                    <strong><i>this</i></strong> keyword cannot be used in static method but can be used in non-static method
-                  </li>
-                  <li>
-                    Every class must have at least one constructor
-                  </li>
-                  <li>
-                    If we do not provide any constructors in a class's declaration, then the compiler creates a default constructor that takes no arguments when it is invoked
-                  </li>
-                  <li>
-                    <strong><i>super</i></strong> keyword can be used to access superclass variables
+                    <strong>
+                      <i>super</i>
+                    </strong>{' '}
+                    keyword can be used to access superclass variables
                   </li>
                 </ul>
               </li>
@@ -94,12 +123,15 @@ const OopsConceptComponent = () => {
           <h4 className='oopsConceptComponent__titleTwo'>Polymorphism</h4>
           <ul>
             <li>
-              It enables us to <i>program in the general</i> rather than <i>program in specific</i>
+              It enables us to <i>program in the general</i> rather than{' '}
+              <i>program in specific</i>
             </li>
           </ul>
         </div>
       </div>
-      <Link className='oopsConceptComponent__link' to='/'>Back to Home</Link>
+      <Link className='oopsConceptComponent__link' to='/'>
+        Back to Home
+      </Link>
     </div>
   );
 };
