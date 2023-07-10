a1ed8c87c6869cc6e2d7273724b5e9e9b887eae7
karananeja <karananeja.r@gmail.com>
2023-07-10 20:43:30 +0530

    updated the spelling error

--- src/pages/Axios.js
+++ src/pages/Axios.js
@@ -21,10 +21,10 @@ const AxiosComponent = () => {
             We can send Authorization headers with post request method as well by passing as parameters to the method
           </li>
           <li>
-            Authentication is important for the applications as it provides data and resource protection from unauthorised access to the application
+            Authentication is important for the applications as it provides data and resource protection from unauthorized access to the application
           </li>
           <li>
-            GET Method is generally used when we are trying to retreive the data from the server and POST Method is used when we are trying to submit the data to the server
+            GET Method is generally used when we are trying to retrieve the data from the server and POST Method is used when we are trying to submit the data to the server
           </li>
           <li>
             CORS or Cross Origin Resource Sharing -  is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served
@@ -39,7 +39,7 @@ const AxiosComponent = () => {
             Status code 200 indicates that the request was successful, Status code 404 indicates that the resource could not be found and Status code 500 indicates internal server issue
           </li>
           <li>
-            Axios is promise-based so we can take advantage of async/await in our code and make it simplier and more readable whereas jQuery is callback-based which can make code more difficult to read and understand
+            Axios is promise-based so we can take advantage of async/await in our code and make it simpler and more readable whereas jQuery is callback-based which can make code more difficult to read and understand
           </li>
           <li>
             Axios supports the standard HTTP methods such as GET, POST, DELETE & PUT as well more specialized methods like PATCH & OPTIONS
